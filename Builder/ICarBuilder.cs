19e6585dc50be0c448e4a17f50a63fbd3dc577e9
André N. Darcie <andrendarcie@gmail.com>
2024-04-20 12:24:07 -0300

    Add builder

--- Builder/ICarBuilder.cs
+++
@@ -1,9 +0,0 @@
-public interface ICarBuilder
-{
-    void BuildEngine(string type);
-    void BuildSeats(int number);
-    void BuildGPS(bool hasGPS);
-    void BuildTyres(string type);
-    void PaintColor(string color);
-    Car GetResult();
-}
