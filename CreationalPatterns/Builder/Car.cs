85263fc10073c285a71d156a6744e40a7da27e4c
André N. Darcie <andrendarcie@gmail.com>
2024-05-03 12:54:11 -0300

    Update Car.cs

--- CreationalPatterns/Builder/Car.cs
+++ CreationalPatterns/Builder/Car.cs
@@ -1,10 +1,10 @@
 public class Car
 {
-    public string Engine { get; set; }
+    public string Engine { get; set; } = "";
     public int SeatCount { get; set; }
     public bool HasGPS { get; set; }
-    public string Tyres { get; set; }
-    public string Color { get; set; }
+    public string Tyres { get; set; } = "";
+    public string Color { get; set; } = "";
 
     public override string ToString()
     {
